b47790e54e145e9ee4fcb3c450839b8ecfc87d6f
Andrew Ford <andrew@andrewford.co.nz>
2024-08-26 21:50:27 +1200

    Updated page6 styling. Added httpyac

--- server/listing/listing.repository.js
+++ server/listing/listing.repository.js
@@ -1,44 +1,44 @@
 const db = require("../db");
 
 module.exports = {
-	getListings: async () => {
-		try {
-			const result = await db.query(
-				`SELECT l.id, l.title, l.sub_title, l.listing_description, l.listing_price, l.condition_new, c."name" AS category FROM listings l INNER JOIN categories c ON c.id = l.category_id 
+  getListings: async () => {
+    try {
+      const result = await db.query(
+        `SELECT l.id, l.title, l.sub_title, l.listing_description, l.listing_price, l.condition_new, c."name" AS category FROM listings l INNER JOIN categories c ON c.id = l.category_id
         `,
-			);
-			return result.rows;
-		} catch (error) {
-			throw Error(error);
-		}
-	},
-	addListing: async (listingDetails) => {
-		try {
-			const result = await db.query(
-				`INSERT INTO listings (title, category_id, sub_title, end_date, listing_description, condition_new, images, hero_image, listing_price, reserve_price, credit_card_payment, bank_transfer_payment, bitcoin_payment, pick_up, shipping_option)
+      );
+      return result.rows;
+    } catch (error) {
+      throw Error(error);
+    }
+  },
+  addListing: async (listingDetails) => {
+    try {
+      const result = db.query(
+        `INSERT INTO listings (title, category_id, sub_title, end_date, listing_description, condition_new, images, hero_image, listing_price, reserve_price, credit_card_payment, bank_transfer_payment, bitcoin_payment, pick_up, shipping_option)
 VALUES ($1, $2, $3, $4, $5, $6, $7);
         `,
-				[
-					listingDetails.titleCategory.title,
-					listingDetails.titleCategory.categoryId,
-					listingDetails.titleCategory.subTitle,
-					listingDetails.titleCategory.endDate,
-					listingDetails.itemDetails.listingDescription,
-					listingDetails.itemDetails.conditionNew,
-					listingDetails.photos.images,
-					listingDetails.photos.heroImage,
-					listingDetails.pricePayment.listingPrice,
-					listingDetails.pricePayment.reservePrice,
-					listingDetails.pricePayment.creditCardPayment,
-					listingDetails.pricePayment.bankTransferPayment,
-					listingDetails.pricePayment.bitcoinPayment,
-					listingDetails.shipping.pickUp,
-					listingDetails.shipping.shippingOption,
-				],
-			);
-			return result.rows;
-		} catch (error) {
-			throw Error(error);
-		}
-	},
+        [
+          listingDetails.titleCategory.title,
+          listingDetails.titleCategory.categoryId,
+          listingDetails.titleCategory.subTitle,
+          listingDetails.titleCategory.endDate,
+          listingDetails.itemDetails.listingDescription,
+          listingDetails.itemDetails.condition,
+          listingDetails.photos.images,
+          listingDetails.photos.heroImage,
+          listingDetails.pricePayment.listingPrice,
+          listingDetails.pricePayment.reservePrice,
+          listingDetails.pricePayment.creditCardPayment,
+          listingDetails.pricePayment.bankTransferPayment,
+          listingDetails.pricePayment.bitcoinPayment,
+          listingDetails.shipping.pickUp,
+          listingDetails.shipping.shippingOption,
+        ],
+      );
+      return result.rows;
+    } catch (error) {
+      throw Error(error);
+    }
+  },
 };
