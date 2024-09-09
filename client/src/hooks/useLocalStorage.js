fff0ea76759a110990bc0fe3c677d77d06708b81
Andrew Ford <andrew@andrewford.co.nz>
2024-09-10 10:29:17 +1200

    Added changes to launch to open default chrome profile Added JSDoc comments for Local/Session storage functions

--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -1,25 +1,37 @@
-import { useState, useEffect } from "react";
-
-function useLocalStorage(key, defaultValue) {
-	const [value, setValue] = useState(() => {
-		let currentValue;
-
-		try {
-			currentValue = JSON.parse(
-				localStorage.getItem(key) || String(defaultValue),
-			);
-		} catch (error) {
-			currentValue = defaultValue;
-		}
-
-		return currentValue;
-	});
-
-	useEffect(() => {
-		localStorage.setItem(key, JSON.stringify(value));
-	}, [value, key]);
-
-	return [value, setValue];
+/**
+ * Function to set an item in local storage.
+ *
+ * @param {string} key - The name of the key for the item being saved.
+ * @param {*} value - The value that will be saved under the given key.
+ * @returns {boolean} True if the save was successful, false otherwise.
+ */
+function setLocalStorageItem(key, value) {
+	return localStorage.setItem(key, JSON.stringify(value));
 }
 
-export default useLocalStorage;
+/**
+ * Function to get an item from local storage.
+ *
+ * @param {string} key - The name of the key for the item being retrieved.
+ * @returns {*} The item stored under the given key or undefined if it does not exist.
+ */
+function getLocalStorageItem(key) {
+	const item = localStorage.getItem(key);
+
+	if (item === null) return undefined;
+
+	return JSON.parse(item);
+}
+
+/**
+ * Function to remove an item from local storage.
+ *
+ * @param {string} key - The name of the key for the item being removed.
+ * @returns {boolean} True if the item was successfully removed, false otherwise.
+ */
+function removeLocalStorageItem(key) {
+	returnlocalStorage.removeItem(key);
+	return true;
+}
+
+export { setLocalStorageItem, getLocalStorageItem, removeLocalStorageItem };
